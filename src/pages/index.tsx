90ff1b5afceaee9480efa784283eadf77761ecb3
Wallace Sá Lima <wallacealmeida@gmail.com>
2022-10-03 21:15:49 -0300

    subindo projeto de cadastro

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className={`
+    flex h-screen justify-center items-center bg-gradient-to-r from-purple-500 via-cyan-300   to-blue-600
+    `}>
+      <span className='text-4xl'>Hello world!</span>
+    </div>
+  )
+}
